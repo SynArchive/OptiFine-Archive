29a3533c297105b92475fd2cac77a219f01099af
Synergetic00 <92153752+SynArchive@users.noreply.github.com>
2022-08-19 11:34:59 +1000

    1.19

--- update.py
+++ update.py
@@ -2,11 +2,14 @@ import json
 import os
 import re
 import requests
+import time
 
 BMCLAPI2_VERSION_LIST = 'https://bmclapi2.bangbang93.com/optifine/versionList'
 
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
+
 def download_from_bmclapi2():
-    response1 = requests.get(BMCLAPI2_VERSION_LIST)
+    response1 = requests.get(BMCLAPI2_VERSION_LIST, headers=headers)
     rjson = json.loads(response1.content)
     for entry in rjson:
         jar_url = f'https://bmclapi2.bangbang93.com/optifine/{entry["mcversion"]}/{entry["type"]}/{entry["patch"]}'
@@ -14,10 +17,11 @@ def download_from_bmclapi2():
             os.makedirs(f'{entry["mcversion"]}')
         file_path = f'{entry["mcversion"]}/{entry["filename"]}'
         if not os.path.isfile(file_path):
-            response2 = requests.get(jar_url)
+            response2 = requests.get(jar_url, headers=headers)
             with open(file_path, 'wb') as f:
                 f.write(response2.content)
                 print(f'Downloaded {entry["filename"]}')
+            time.sleep(5)
 
 if __name__ == '__main__':
     download_from_bmclapi2()
